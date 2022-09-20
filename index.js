3ec2efe9c7ee0b57f8a8750ff9bfbf5523f0b4c3
Whotislove <vlad.lobunets@mail.ru>
2022-09-20 22:57:58 +0300

    add jwt and post/get

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-console.log('back');
-console.log('suda');
+import express from 'express';
+import jwt from 'jsonwebtoken';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send(' 1112 Hello world');
+});
+
+app.post('/auth/login', (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: 'ASad adsd',
+    },
+    'secret123',
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('Server ok');
+});
